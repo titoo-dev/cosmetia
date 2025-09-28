d2a5d0315cf85c932d4da854f4b2a94e85cc83a5
titoo-dev <dev.titosy@gmail.com>
2025-09-28 17:23:45 +0300

    Implement user redirection based on authentication status in Home page
    - Updated the `Home` component to be asynchronous and fetch the current user using `getCurrentUserAction`.
    - Added a redirect to the marketplace for authenticated users, enhancing navigation based on user state.
    - Maintained existing layout structure while improving user experience through dynamic routing.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,26 @@
+import { getCurrentUserAction } from "@/actions/get-current-user-action";
 import { AIAssistantSection } from "@/components/landing/ai-assistant-section";
 import { FeaturesSection } from "@/components/landing/feature-section";
 import { HeroSection } from "@/components/landing/hero-section";
 import { TargetAudienceSection } from "@/components/landing/target-audience-section";
+import { redirect } from "next/navigation";
 
 export const dynamic = 'force-dynamic'
 
-export default function Home() {
-  return (
+export default async function Home() {
+
+	const currentUser = await getCurrentUserAction();
+
+	if (currentUser) {
+		return redirect('/customer/marketplace');
+	}
+
+	return (
 		<div className="min-h-screen bg-white">
 			<HeroSection />
 			<FeaturesSection />
 			<TargetAudienceSection />
 			<AIAssistantSection />
 		</div>
-  );
+	);
 }
